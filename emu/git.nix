2dfd080b7a061cdef46f9b4a0cd3334a44add5e9
Jordan Bravo <jordan@bravo.cc>
2023-08-24 18:28:47 -0400

    Add emu directory

---
+++ emu/git.nix
@@ -0,0 +1,51 @@
+{
+  programs.git = {
+    aliases = {
+      # Deletes branches that have been mergd into main, qa, or dev
+      tidy = "!git branch --merged | grep -vE \"main|qa|dev|\\*\" | xargs -n 1 git branch -d";
+    };
+    delta = {
+      enable = true;
+    };
+    enable = true;
+    extraConfig = {
+      core = {
+        editor = "nvim";
+        excludesfile = "~/.gitignore-global";
+      };
+      delta = {
+        navigate = true;
+        light = false;
+        side-by-side = true;
+      };
+      diff = {
+        tool = "nvimdiff";
+        colorMoved = "default";
+      };
+      merge = {
+        conflictstyle = "diff3";
+      };
+      mergetool.nvimdiff = {
+        layout = "(LOCAL,REMOTE)/MERGED";
+      };
+      pull = {
+        ff = "only";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+    };
+    includes = [
+      {
+        path = "~/bd/.gitconfig-bd";
+        condition = "gitdir:~/bd";
+      }
+    ];
+    userName = "Jordan Bravo";
+    userEmail = "jordan@bravo.cc";
+    signing = {
+      key = "E11FA432F965854B";
+      signByDefault = true;
+    };
+  }; 
+}
