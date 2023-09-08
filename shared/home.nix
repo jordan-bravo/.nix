ada150afca9846e0fdf3196129f656a65117b78a
Jordan Bravo <jordan@bravo.cc>
2023-09-08 14:59:23 -0400

    home.nix

--- shared/home.nix
+++ shared/home.nix
@@ -64,6 +64,8 @@
       neovim # Text editor
       nil # Language server for Nixlang
       nixpkgs-fmt # Formatter for Nixlang
+      nodePackages.pyright
+      nodePackages.typescript-language-server
       onefetch # Git repo summary
       obsidian # Note-taking
       # rust-analyzer # Rust LSP
