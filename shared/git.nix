2b5f51332ad1e8d17178be04b1118839db6e2182
Jordan Bravo <jordan@bravo.cc>
2023-09-23 10:18:28 -0400

    create git alias for branches

--- shared/git.nix
+++ shared/git.nix
@@ -4,6 +4,7 @@
     aliases = {
       # Deletes branches that have been mergd into main, qa, or dev
       tidy = "!git branch --merged | grep -vE \"main|qa|dev|\\*\" | xargs -n 1 git branch -d";
+      br = "!git for-each-ref refs/heads --color=always --sort -committerdate --format='%(HEAD)%(color:reset);%(color:yellow)%(refname:short)%(color:reset);%(contents:subject);%(color:green)(%(committerdate:relative))%(color:blue);<%(authorname)>' | column -t -s ';'";
     };
     delta = {
       enable = true;
