7fe93891ed86d25e7344f8f97c1a05e54cbed9ac
Jordan Bravo <jordan@bravo.cc>
2024-03-21 22:52:56 -0400

    updates

--- carby/configuration.nix
+++ carby/configuration.nix
@@ -42,21 +42,18 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+  services = {
+    printing.enable = true;
+    tailscale.enable = true;
+    xserver = {
+      enable = true;
+      # Enable Gnome
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      xkb.layout = "us";
+      xkb.options = "caps:escape_shifted_capslock";
+    };
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
