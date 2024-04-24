ce9bfe3574c7cdd3cab3f9b97ca4488cf89a3377
Jordan Bravo <jordan@bravo.cc>
2024-04-23 20:10:42 -0400

    enhance delta highlighting

--- home-manager/shared/git.nix
+++ home-manager/shared/git.nix
@@ -13,6 +13,10 @@
         navigate = true;
         light = false;
         side-by-side = true;
+        plus-style = "syntax \"#003500\"";
+        plus-emph-style = "syntax bold \"#007e5e\"";
+        minus-style = "syntax \"#5e0000\"";
+        minus-emph-style = "syntax bold \"#80002a\"";
       };
     };
     enable = true;
