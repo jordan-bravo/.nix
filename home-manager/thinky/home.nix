08a07625333da1cd7e297c56461f2690aa781846
Jordan Bravo <jordan@bravo.cc>
2024-06-13 08:31:32 -0400

    thinky: set text scaling for built-in display

--- home-manager/thinky/home.nix
+++ home-manager/thinky/home.nix
@@ -13,10 +13,10 @@
       unicode-hotkey = [ ];
     };
     "org/gnome/desktop/interface" = {
-      text-scaling-factor = 0.8; # BitLab LG
+      # text-scaling-factor = 0.8; # BitLab LG
       # text-scaling-factor = 1.0;
       # text-scaling-factor = 1.15; # Home Innocn
-      # text-scaling-factor = 1.5; # thinky built-in
+      text-scaling-factor = 1.5; # thinky built-in
     };
     "org/gnome/desktop/peripherals/mouse" = {
       speed = -0.6;
