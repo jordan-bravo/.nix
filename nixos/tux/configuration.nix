d8af5b46a4d5b2b401c4d5773aad10743c9e9fa1
Jordan Bravo <jordan@bravo.cc>
2024-07-09 16:57:29 -0400

    tux: enable ollama

--- nixos/tux/configuration.nix
+++ nixos/tux/configuration.nix
@@ -48,6 +48,15 @@
     port = 6379;
   };
 
+  services.ollama = {
+    enable = true;
+  };
+
+  services.nextjs-ollama-llm-ui = {
+    enable = true;
+    ollamaUrl = "http://127.0.0.1:11434";
+  };
+
   networking.hostName = "tux";
   # networking.enableIPv6 = false;
 
