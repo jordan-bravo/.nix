f4b8ec8142cca457ca7f3caa56d65a630e86dbeb
Jordan Bravo <jordan@bravo.cc>
2024-12-13 15:42:53 -0500

    move nvim deps into separate file

--- home-manager/lenny/home.nix
+++ home-manager/lenny/home.nix
@@ -12,46 +12,7 @@
       neovim # Text editor / IDE
       nixgl.nixGLIntel # Helps some Nix packages run on non-NixOS
       udiskie # Removable disk automounter for udisks
-
-      # Neovim dependencies nvim-dep
-      delve # Debugger for the Go programming language
-      dockerfile-language-server-nodejs # A language server for Dockerfiles # nvim-dep
-      emmet-ls # Emmet support based on LSP # nvim-dep
-      eslint_d # ESLint daemon for increased performance # nvim-dep
-      fzf # nvim-dep
-      gcc # GNU Compiler Collection # nvim-dep # nvim-dep
-      gnumake # A tool to control the generation of non-source files from sources # nvim-dep
-      gopls # Official language server for Go / Golang # nvim-dep
-      hadolint # Dockerfile Linter JavaScript API # nvim-dep
-      lazygit # Simple terminal UI for git commands # nvim-dep
-      lua-language-server # LSP language server for Lua # nvim-dep
-      luajit # JIT compiler for Lua 5.1 # nvim-dep
-      luajitPackages.jsregexp # JavaScript (ECMA19) regular expressions for lua # nvim-dep
-      luajitPackages.luacheck # A static analyzer & linter for Lua # nvim-dep
-      marksman # Language Server for markdown # nvim-dep
-      markdownlint-cli # Command line interface for MarkdownLint
-      nil # Nix langauge server # nvim-dep
-      nixd # Nix langauge server # nvim-dep
-      nixpkgs-fmt # Formatter for Nixlang # nvim-dep
-      nodejs_22 # Event-driven I/O framework for the V8 JavaScript engine # nvim-dep
-      nodePackages.bash-language-server # A language server for Bash
-      nodePackages.eslint # An AST-based pattern checker for JavaScript
-      nodePackages.prettier # nvim-dep
-      python312Packages.debugpy # Implementation of the Debug Adapter Protocol for Python
-      pyright # Python static type checker # nvim-dep
-      nodePackages.typescript # nvim-dep
-      nodePackages.typescript-language-server # nvim-dep
-      prettierd # Prettier daemon for faster formatting # nvim-dep
-      # python312 # Python 3.12 # nvim-dep
-      rustfmt # Rust formatter # nvim-dep
-      ruff # An extremely fast Python linter # nvim-dep
-      ruff-lsp # Ruff LSP for Python # nvim-dep
-      rust-analyzer # Modular compiler frontend for the Rust language
-      stylua # Lua code formatter # nvim-dep
-      tailwindcss-language-server # LSP functionality for Tailwind CSS # nvim-dep
-      unzip # An extraction utility for archives compressed in .zip format # nvim-dep
-      vscode-langservers-extracted # HTML/CSS/JSON/ESLint language servers extracted from vscode # nvim-dep
-      yaml-language-server # Language Server for YAML Files
+      wl-clipboard # Wayland clipboard utilities, wl-copy and wl-paste
     ];
     homeDirectory = "/home/${config.home.username}";
     sessionVariables = {
@@ -64,13 +25,15 @@
     username = "jordan";
   };
   imports = [
-    ../shared/fuzzel.nix
+    # ../shared/fuzzel.nix
     ../shared/git.nix
     # ../shared/home.nix
     # ../shared/kanata.nix
     ../shared/kanshi.nix
+    ../shared/nvim/deps.nix
     ../shared/ripgrep.nix
     # ../shared/workstations.nix
+    ../../secrets/workstations-secrets.nix
     # ../shared/zellij.nix
     # ../shared/zsh.nix
   ];
@@ -133,6 +96,7 @@
       nix-direnv.enable = true;
     };
     fzf.enable = true;
+    fuzzel.enable = true;
     gh.enable = true;
     # gpg.enable = true;
     home-manager.enable = true;
@@ -161,6 +125,9 @@
           disabled = true;
         };
       };
+    };
+    wofi = {
+      enable = true;
     };
     # yazi = {
     #   enable = true; # disabled because broken by latest update to nixpkgs on Nov 3rd
