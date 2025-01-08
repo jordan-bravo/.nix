ee59eabd047b12c4448bbd8c388c78c90ae3fc28
Jordan Bravo <jordan@bravo.cc>
2025-01-08 12:28:27 -0500

    add commented out sway-session.target

--- home-manager/lenny/home.nix
+++ home-manager/lenny/home.nix
@@ -142,6 +142,17 @@
       tray = "never";
     };
   };
+  # systemd.user.services = {
+  #   sway-session = {
+  #     Unit = {
+  #       Description = "sway compositor session";
+  #       Documentation = "man:systemd.special(7)";
+  #       BindsTo = "graphical-session.target";
+  #       Wants = "graphical-session-pre.target";
+  #       After = "graphical-session-pre.target";
+  #     };
+  #   };
+  # };
   targets.genericLinux.enable = true;
   xdg = {
     enable = true;
