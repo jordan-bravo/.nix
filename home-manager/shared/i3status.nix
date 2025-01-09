78fc0a52ed1662533f4b42c9d7f1f85ad62f5fef
Jordan Bravo <jordan@bravo.cc>
2025-01-09 15:36:00 -0500

    updates

--- home-manager/shared/i3status.nix
+++ home-manager/shared/i3status.nix
@@ -4,15 +4,18 @@
       blocks = [
         {
           block = "battery";
+          device = "BAT0";
+          interval = 1;
+          format = " $icon $percentage ";
         }
-        {
-          alert = 10.0;
-          block = "disk_space";
-          info_type = "available";
-          interval = 60;
-          path = "/";
-          warning = 20.0;
-        }
+        # {
+        #   alert = 10.0;
+        #   block = "disk_space";
+        #   info_type = "available";
+        #   interval = 60;
+        #   path = "/";
+        #   warning = 20.0;
+        # }
         # {
         #   block = "memory";
         #   format = " $icon mem_used_percents ";
@@ -22,17 +25,17 @@
           block = "cpu";
           interval = 1;
         }
-        {
-          block = "load";
-          format = " $icon $1m ";
-          interval = 1;
-        }
+        # {
+        #   block = "load";
+        #   format = " $icon $1m ";
+        #   interval = 1;
+        # }
         {
           block = "sound";
         }
         {
           block = "time";
-          format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+          format = " $timestamp.datetime(f:'%a %b %d %R') ";
           interval = 60;
         }
       ];
