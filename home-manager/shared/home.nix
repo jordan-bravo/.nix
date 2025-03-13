34d4d8b192095281dae61e664c7fd1aefb3ccf90
Jordan Bravo <jordan@bravo.cc>
2025-03-13 19:59:30 +0000

    comment out duplicate nvim deps

--- home-manager/shared/home.nix
+++ home-manager/shared/home.nix
@@ -41,7 +41,7 @@
       bottom # A cross-platform graphical process/system monitor with a customizable interface
       browsh # Fully-modern text-based browser, rendering to TTY and browsers
       # busybox # Tiny versions of common UNIX utilities in a single small executable
-      cargo # Rust package manager
+      # cargo # Rust package manager
       dante # Circuit-level SOCKS client/server that can be used to provide convenient and secure network connectivity # aerc-dep
       delta # A syntax-highlighting pager for git
       dhcping # Send DHCP request to find out if a DHCP server is running
@@ -105,45 +105,45 @@
       # zola # A fast static site generator with everything built-in
 
       # Neovim dependencies nvim-dep
-      basedpyright # Type checker for the Python language
-      delve # Debugger for the Go programming language
-      dockerfile-language-server-nodejs # A language server for Dockerfiles # nvim-dep
-      emmet-ls # Emmet support based on LSP # nvim-dep
-      eslint_d # ESLint daemon for increased performance # nvim-dep
-      fzf # nvim-dep
+      # basedpyright # Type checker for the Python language
+      # delve # Debugger for the Go programming language
+      # dockerfile-language-server-nodejs # A language server for Dockerfiles # nvim-dep
+      # emmet-ls # Emmet support based on LSP # nvim-dep
+      # eslint_d # ESLint daemon for increased performance # nvim-dep
+      # fzf # nvim-dep
       # gcc # GNU Compiler Collection # nvim-dep # nvim-dep
       # gnumake # A tool to control the generation of non-source files from sources # nvim-dep
-      gopls # Official language server for Go / Golang # nvim-dep
-      hadolint # Dockerfile Linter JavaScript API # nvim-dep
-      lazygit # Simple terminal UI for git commands # nvim-dep
-      lua-language-server # LSP language server for Lua # nvim-dep
-      luajit # JIT compiler for Lua 5.1 # nvim-dep
-      luajitPackages.jsregexp # JavaScript (ECMA19) regular expressions for lua # nvim-dep
-      luajitPackages.luacheck # A static analyzer & linter for Lua # nvim-dep
-      marksman # Language Server for markdown # nvim-dep
-      markdownlint-cli # Command line interface for MarkdownLint
-      nil # Nix langauge server # nvim-dep
-      nixd # Nix langauge server # nvim-dep
-      nixpkgs-fmt # Formatter for Nixlang # nvim-dep
-      nodejs_22 # Event-driven I/O framework for the V8 JavaScript engine # nvim-dep
-      nodePackages.bash-language-server # A language server for Bash
-      nodePackages.eslint # An AST-based pattern checker for JavaScript
-      nodePackages.prettier # nvim-dep
-      python312Packages.debugpy # Implementation of the Debug Adapter Protocol for Python
-      pyright # Python static type checker # nvim-dep
-      nodePackages.typescript # nvim-dep
-      nodePackages.typescript-language-server # nvim-dep
-      prettierd # Prettier daemon for faster formatting # nvim-dep
+      # gopls # Official language server for Go / Golang # nvim-dep
+      # hadolint # Dockerfile Linter JavaScript API # nvim-dep
+      # lazygit # Simple terminal UI for git commands # nvim-dep
+      # lua-language-server # LSP language server for Lua # nvim-dep
+      # luajit # JIT compiler for Lua 5.1 # nvim-dep
+      # luajitPackages.jsregexp # JavaScript (ECMA19) regular expressions for lua # nvim-dep
+      # luajitPackages.luacheck # A static analyzer & linter for Lua # nvim-dep
+      # marksman # Language Server for markdown # nvim-dep
+      # markdownlint-cli # Command line interface for MarkdownLint
+      # nil # Nix langauge server # nvim-dep
+      # nixd # Nix langauge server # nvim-dep
+      # nixpkgs-fmt # Formatter for Nixlang # nvim-dep
+      # nodejs_22 # Event-driven I/O framework for the V8 JavaScript engine # nvim-dep
+      # nodePackages.bash-language-server # A language server for Bash
+      # nodePackages.eslint # An AST-based pattern checker for JavaScript
+      # nodePackages.prettier # nvim-dep
+      # python312Packages.debugpy # Implementation of the Debug Adapter Protocol for Python
+      # pyright # Python static type checker # nvim-dep
+      # nodePackages.typescript # nvim-dep
+      # nodePackages.typescript-language-server # nvim-dep
+      # prettierd # Prettier daemon for faster formatting # nvim-dep
       # python312 # Python 3.12 # nvim-dep
-      rustfmt # Rust formatter # nvim-dep
-      ruff # An extremely fast Python linter # nvim-dep
-      ruff-lsp # Ruff LSP for Python # nvim-dep
-      rust-analyzer # Modular compiler frontend for the Rust language
-      stylua # Lua code formatter # nvim-dep
-      tailwindcss-language-server # LSP functionality for Tailwind CSS # nvim-dep
-      unzip # An extraction utility for archives compressed in .zip format # nvim-dep
-      vscode-langservers-extracted # HTML/CSS/JSON/ESLint language servers extracted from vscode # nvim-dep
-      yaml-language-server # Language Server for YAML Files
+      # rustfmt # Rust formatter # nvim-dep
+      # ruff # An extremely fast Python linter # nvim-dep
+      # ruff-lsp # Ruff LSP for Python # nvim-dep
+      # rust-analyzer # Modular compiler frontend for the Rust language
+      # stylua # Lua code formatter # nvim-dep
+      # tailwindcss-language-server # LSP functionality for Tailwind CSS # nvim-dep
+      # unzip # An extraction utility for archives compressed in .zip format # nvim-dep
+      # vscode-langservers-extracted # HTML/CSS/JSON/ESLint language servers extracted from vscode # nvim-dep
+      # yaml-language-server # Language Server for YAML Files
 
       # Dependencies of openfortivpn
       # autoconf # Part of the GNU Build System
