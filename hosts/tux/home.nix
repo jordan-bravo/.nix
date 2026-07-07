b68dc9df11c91d532c8da80b371e358290a4dcbd
Jordan Bravo <jordan@bravo.cc>
2026-07-07 13:47:27 -0400

    tux: disable gnome notification sound for charging cable

--- hosts/tux/home.nix
+++ hosts/tux/home.nix
@@ -5,4 +5,22 @@
   ];
 
   services.trayscale.enable = true;
+
+  # GNOME Specific:
+  # The following disables the notification sound that plays in GNOME when
+  # a charging cable is plugged in or unplugged.
+  # Sound theme that inherits everything from the default but disables the
+  # charger plug/unplug sounds (a "<name>.disabled" file mutes that event).
+  xdg.dataFile = {
+    "sounds/__custom/index.theme".text = ''
+      [Sound Theme]
+      Name=Custom
+      Inherits=freedesktop
+      Directories=.
+    '';
+    "sounds/__custom/power-plug.disabled".text = "";
+    "sounds/__custom/power-unplug.disabled".text = "";
+  };
+
+  dconf.settings."org/gnome/desktop/sound".theme-name = "__custom";
 }
